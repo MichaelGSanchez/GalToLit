716b47024dcc1970ca3a3a957d0e7f4f4029d978
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-01-22 21:41:12 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Gal.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive;
+
+public class Gal {
+
+  public static void main(String[] args) {
+    double gallons; //start with 10 gallons
+    double liters; //holds conversion wo liters
+
+    gallons = 10; //start with 10 gallons
+    liters = gallons * 3.7854; //convert to liters
+    System.out.println(gallons+" gallons is " + liters + " liters ");
+  }
+
+}
